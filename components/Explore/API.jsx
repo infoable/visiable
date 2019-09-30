d9f6b47adfda578311e361def5aaebd6a67b7fa5
Doyeong <jeekc0308@gmail.com>
2019-09-30 18:16:05 +0900

    available Actions

--- components/Explore/API.jsx
+++ components/Explore/API.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link';
 import Button from '../Form/Button';
 
-const APIComponent = ({ site, _id, by }) => {
+const APIComponent = ({ site, _id, by, actions }) => {
     return (
         <div style={{
             padding: "1em",
@@ -16,6 +16,17 @@ const APIComponent = ({ site, _id, by }) => {
                 marginBottom: '1em'
             }}>{site}</h3>
             {by && <div style={{ marginBottom: 10 }}>{by.username}님이 만듦</div>}
+            {
+                actions &&
+                <div>
+                    <h5>사용 가능한 액션</h5>
+                    <ul>
+                        {actions.map((d, i) => (
+                            <li key={i}>{d.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            }
             {!by && <Link href={"/visiable/" + _id}><Button filled>편집하기</Button></Link>}
         </div>
     )
